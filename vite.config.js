5fd29d21e255ba1e429ce517c982b660882777ff
Nishanthan R <nishanthanmanimala@gmail.com>
2025-03-20 14:05:53 +0530

    dep

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Adjust this if you're using custom aliases
+    },
+  },
+});
