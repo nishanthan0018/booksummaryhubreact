b2f357b28f4954a7a24025f3b9618afe2c4ff274
nishanthan0018 <166122270+nishanthan0018@users.noreply.github.com>
2025-03-20 14:19:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // Adjust this if you're using custom aliases
-    },
-  },
+  
 });
